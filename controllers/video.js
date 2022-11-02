249c5e2e7d81a1f77d6f1c1a946beeff698ac8b2
Prajwal Ladkat <praju.ladkat@gmail.com>
2022-11-02 22:16:12 +0530

    updated videos api

--- controllers/video.js
+++ controllers/video.js
@@ -90,9 +90,7 @@ export const getVideo = async (req, res) => {
 export const getVideos = async (req, res) => {
     try {
         const videos = await Video.find({});
-        res.json(createError('Success', 200, 'All videos', videos));
         return res.status(200).json(videos);
-        // -
     } catch (error) {
         res.status(500).json(createError('Failed', 500, 'Server Error', null));
     }
