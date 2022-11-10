0bed3a92f6854aa6e056561c074b28c1350b5b84
Prajwal Ladkat <praju.ladkat@gmail.com>
2022-11-10 13:40:15 +0530

    modified cors settings

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const privteVapidKey = 'IT0TZT0AUicBHyKUZrK9zhDvGMoX-qYoe6i00PCGUqY';
 webpush.setVapidDetails('mailto:iamprajwalladkat@gmail.com', publicVapidKey, privteVapidKey);
 
 app.use(cors({
-    origin: '*',
+    origin: 'http://localhost:3000',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
